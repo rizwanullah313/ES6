2fdd4e193047ee399b088a16122ffe6c2d869de7
rizwanullah313 <ru323091@gmail.com>
2022-05-15 19:16:52 +0500

    Generator

--- style.js
+++ style.js
@@ -369,3 +369,31 @@ getData('GET','https://jsonplaceholder.typicode.com/todos').then(function(data)
 
 
 
+//////////////////// Generator ////////////////////
+
+function *g1()
+{
+    console.log('Hello');
+    yield 'Yield 1 Ran..';
+    console.log('World');
+    yield 'Yield 2 Ran...';
+
+    // false to ture need to somethings return
+
+    return "returned";
+}
+
+var g = g1();
+// console.log(g.next().value);
+// console.log(g.next());
+// console.log(g.next());
+
+//console.log(g.next().value);
+//console.log(g.next().value);
+//console.log(g.next().value);
+
+
+for(let val of g)
+{
+    console.log(val);
+}
